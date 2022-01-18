9e27c6b331c7b824cac3cfc6756ee8e4a6df609d
scottschmidl <sjschmidl@gmail.com>
2022-01-18 11:11:12 -0600

    minor edits, cleaned up wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -10,11 +10,9 @@ class Wrangle:
             pandas dataframe: used for analysis and modeling
         """
         filename = "MyFoodData Nutrition Facts SpreadSheet Release 1.4 - SR Legacy and FNDDS.csv"
-        #filename = "MyFoodData Nutrition Facts SpreadSheet Release 1.4.xlsx"
 
         try:
             return pd.read_csv(filename)
-            #return pd.read_excel(filename, sheet_name="SR Legacy and FNDDS")
 
         except FileNotFoundError:
             print(f"{filename} not found. Please go to https://tools.myfooddata.com/nutrition-facts-database-spreadsheet.php to download the file.")
