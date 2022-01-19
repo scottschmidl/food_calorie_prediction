32b72bc1bb5303a153497144ad8858007e202bc1
scottschmidl <sjschmidl@gmail.com>
2022-01-19 09:18:52 -0600

    added metric method

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from prepare import Prepare
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.linear_model import LinearRegression, LassoCV, RidgeCV
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
 class Models:
 
@@ -12,8 +13,11 @@ class Models:
     train, validate, test = sgs.split(nutrition_facts)
     (X_train, y_train), (X_validate, y_validate), (X_test, y_test) = sgs.get_Xy(train, validate, test, target_col="calories", cols_drop=["calories"])
 
-    def fit_supervised(X_train, y_train):
+    def fit_supervised(self, X_train, y_train):
         pass
 
-    def fit_unsupervised(X_train, y_train):
+    def fit_unsupervised(self, X_train, y_train):
         pass
+
+    def get_metric(self, y_train, y_pred):
+        return mean_squared_error(y_train, y_pred)
