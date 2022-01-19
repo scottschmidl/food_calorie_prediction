42dd6b986f966e88690f01c7fe7908b967731d88
scottschmidl <sjschmidl@gmail.com>
2022-01-19 09:17:14 -0600

    added imports and starts class

---
+++ models.py
@@ -0,0 +1,19 @@
+from split_get_scale import SplitGetScale
+from prepare import Prepare
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.linear_model import LinearRegression, LassoCV, RidgeCV
+from sklearn.ensemble import RandomForestRegressor
+
+class Models:
+
+    nutrition_facts = Prepare().get_food_prep()
+
+    sgs = SplitGetScale()
+    train, validate, test = sgs.split(nutrition_facts)
+    (X_train, y_train), (X_validate, y_validate), (X_test, y_test) = sgs.get_Xy(train, validate, test, target_col="calories", cols_drop=["calories"])
+
+    def fit_supervised(X_train, y_train):
+        pass
+
+    def fit_unsupervised(X_train, y_train):
+        pass
