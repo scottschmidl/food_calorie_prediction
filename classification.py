b707e7a050fb240563cd66c5d305c51578a21454
scottschmidl <sjschmidl@gmail.com>
2022-01-21 12:37:29 -0600

    finished

--- classification.py
+++ classification.py
@@ -4,6 +4,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 from split_get_scale import SplitGetScale
 from prepare import Prepare
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import accuracy_score
 
 class Models:
 
@@ -18,7 +19,6 @@ class Models:
     def classification_baseline(self):
 
         from pandas import DataFrame
-        from sklearn.metrics import accuracy_score
 
         act_pred_error = DataFrame({"actual": self.y_train})
         act_pred_error["baseline_prediction"] = self.y_train.value_counts().index[0]
@@ -27,33 +27,47 @@ class Models:
 
         print(f"Baseline Accuracy Score:{round(baseline_acc, 2)}")
 
-    def classification_models(self):
+    def classification_models(self, best_model=None):
 
-        # fit the model
-        rfc = RandomForestClassifier().fit(self.X_train_scaled, self.y_train)
-        ada = AdaBoostClassifier().fit(self.X_train_scaled, self.y_train)
-        svc = SVC().fit(self.X_train_scaled, self.y_train)
+        if not best_model:
 
-        # predict on X_train_scaled
-        rfc_pred_train = rfc.predict(self.X_train_scaled)
-        ada_pred_train = ada.predict(self.X_train_scaled)
-        svc_pred_train = svc.predict(self.X_train_scaled)
+            # fit the model
+            rfc = RandomForestClassifier().fit(self.X_train_scaled, self.y_train)
+            ada = AdaBoostClassifier().fit(self.X_train_scaled, self.y_train)
+            svc = SVC().fit(self.X_train_scaled, self.y_train)
 
-        # predict on X_validate_scaled
-        rfc_pred_val = rfc.predict(self.X_validate_scaled)
-        ada_pred_val = ada.predict(self.X_validate_scaled)
-        svc_pred_val = svc.predict(self.X_validate_scaled)
+            # predict on X_train_scaled
+            rfc_pred_train = rfc.predict(self.X_train_scaled)
+            ada_pred_train = ada.predict(self.X_train_scaled)
+            svc_pred_train = svc.predict(self.X_train_scaled)
 
-        # print train RMSE
-        print(f" TRAIN\nRandom Forest Classifier Train Classification Report:\n{classification_report(self.y_train, rfc_pred_train)}\nAda Boost Classifier Train Classification Report:\n{classification_report(self.y_train, ada_pred_train)}\n Support Vector Classifier Train Classification Report:\n{classification_report(self.y_train, svc_pred_train)}\n")
+            # predict on X_validate_scaled
+            rfc_pred_val = rfc.predict(self.X_validate_scaled)
+            ada_pred_val = ada.predict(self.X_validate_scaled)
+            svc_pred_val = svc.predict(self.X_validate_scaled)
 
-        # print validate RMSE
-        print(f"\nVALIDATE\nRandom Forest Classifier Validation Classification Report:\n{classification_report(self.y_validate, rfc_pred_val)}\nAda Boost Classifier Validation Classification Report:\n{classification_report(self.y_validate, ada_pred_val)}\n Support Vector Classifier Validation RMSE:\n{classification_report(self.y_validate, svc_pred_val)}")
+            # print train accuracy
+            print(f"TRAIN\nRandom Forest Classifier Train Accuracy: {accuracy_score(self.y_train, rfc_pred_train)}%\nAda Boost Classifier Train Accuracy: {accuracy_score(self.y_train, ada_pred_train)}%\nSupport Vector Classifier Train Accuracy: {accuracy_score(self.y_train, svc_pred_train)}%")
 
+            # print validate accuracy
+            print(f"VALIDATE\nRandom Forest Classifier Validation Accuracy: {accuracy_score(self.y_validate, rfc_pred_val)}%\nAda Boost Classifier Validation Accuracy: {accuracy_score(self.y_validate, ada_pred_val)}%\nSupport Vector Classifier Validation Accuracy: {accuracy_score(self.y_validate, svc_pred_val)}%")
+
+            return rfc
+
+        else:
+            print("Final Best Model Found! Performing Predictions On X_test_scaled.")
+
+            best_model_pred_test = best_model.predict(self.X_test_scaled)
+
+            print("Calculating Accuracy...")
+
+            print(f"Random Forest Classifier Test Accuracy: {accuracy_score(self.y_test, best_model_pred_test)}")
 
 def main():
-    # Models().classification_models()
+
     Models().classification_baseline()
+    rfc = Models().classification_models()
+    Models().classification_models(best_model=rfc)
 
 if __name__ == "__main__":
     main()
